f2de699c68d401d05e96e1ab8ee35621f5122d87
Eddie <eddietang2314@gmail.com>
2025-12-01 19:00:53 -0600

    Added some instructions

--- quattest.sh
+++ quattest.sh
@@ -1,5 +1,8 @@
 cd gnc
- #  g++ -I. -o main test_ekf.cpp ekf.cpp Madgwick/Madgwick.cpp
+# windows -> msys mingw64
+g++ -I. -o main test_ekf.cpp ekf.cpp Madgwick/Madgwick.cpp
+
+#mac
 g++ -std=c++17 -I. -o main test_ekf.cpp ekf.cpp Madgwick/Madgwick.cpp
 cd ..
 ./gnc/main "./data/MIDAS Trimmed (AL0, CSV).csv" "./output/results.csv"
